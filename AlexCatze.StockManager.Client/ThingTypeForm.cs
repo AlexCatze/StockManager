6c95087c4ac83b86a95bdb51e299b8e344a7ebbe
AlexCatze <alexcatze@gmail.com>
2023-06-04 20:51:26 +0300

    Image support

--- AlexCatze.StockManager.Client/ThingTypeForm.cs
+++ AlexCatze.StockManager.Client/ThingTypeForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace AlexCatze.StockManager.Client
 {
@@ -17,6 +18,15 @@ namespace AlexCatze.StockManager.Client
             InitializeComponent();
             label_box.Text = type.Name;
             description_box.Text = type.Description;//TODO Image support
+            if (!String.IsNullOrEmpty(type.Image))
+            {
+                byte[] data = Convert.FromBase64String(type.Image);
+                if (data != null)
+                {
+                    Bitmap img = new Bitmap(new MemoryStream(data));
+                    pictureBox1.Image = img;
+                }
+            }
         }
 
         private void back_button_Click(object sender, EventArgs e)
